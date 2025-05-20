48174f2ab7feb579c9d7e98ae2870728b083a715
csantilucas <lucascsanti2006@gmail.com>
2025-05-19 21:30:46 -0400

    desenho

--- desenho.js
+++ desenho.js
@@ -4,45 +4,58 @@ for (let y = 0; y < largura; y++) {
     let linha = "";
     
     for (let x = 0; x < largura; x++) {
-        if ( x < 1 || y < 1 || x > 8 || y > 8) {
-            linha += "#";
+        if ( x == y) {
+            linha += "╲";
         } 
-        else {
-            linha += "  ";
+        else if(x + y == largura -1  ) {
+            linha += "╱";
+        }
+        else  {
+            linha += " ";
         }
     }
+    
     console.log(linha);
 }
 
 
 
-
-for (let y = 0; y < 5; y++) {
+largura =5
+for (let y = 0; y < largura; y++) {
     let linha = "";
     
-    for (let x = 0; x < 5; x++) {
-        if ( x < 1 || y < 1 || x > 4 || y > 4) {
-            linha += "#";
+    for (let x = 0; x < largura; x++) {
+        if ( x == y) {
+            linha += "╲";
         } 
-        else {
-            linha += "";
+        else if(x + y == largura -1  ) {
+            linha += "╱";
+        }
+        else  {
+            linha += " ";
         }
     }
     console.log(linha);
 }
 
-
-
-for (let y = 0; y < 2; y++) {
+largura =2
+for (let y = 0; y < largura; y++) {
     let linha = "";
     
-    for (let x = 0; x < 2; x++) {
-        if ( x < 1 || y < 1 || x > 2 || y > 2) {
-            linha += "#";
+    for (let x = 0; x < largura; x++) {
+        if ( x == y) {
+            linha += "╲";
         } 
-        else {
-            linha += "  ";
+        else if(x + y == largura -1  ) {
+            linha += "╱";
+        }
+        else  {
+            linha += " ";
         }
     }
     console.log(linha);
 }
+
+
+
+
