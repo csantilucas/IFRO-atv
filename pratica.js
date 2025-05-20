b987f257cd98e5d6dfe6ce0622bf87011e6cb37e
csantilucas <lucascsanti2006@gmail.com>
2025-05-19 20:56:06 -0400

    pratica

--- pratica.js
+++ pratica.js
@@ -5,13 +5,12 @@ cont = 1;
 quant = 10;
 maior = 0;
 menor = 100;
+sm = 0;
 
 while (cont < quant) {//gera numeros aleatorios de 0 a 100, e verifica se o numero gerado é maior ou menor que os numeros gerados anteriormente, se for maior ele substitui o maior, se for menor ele substitui o menor
     cont++;
     let num = Math.floor(Math.random() * 100);
     console.log('numero gerado é:', num);
-
-
 
     for (let i = 0; i < quant; i++) {
         if (num > maior) {// se num for maior que maior, o maior recebe o num gerado ex: num 20 e maior 10, o maior passa a ser 20
@@ -20,6 +19,8 @@ while (cont < quant) {//gera numeros aleatorios de 0 a 100, e verifica se o nume
         }else if (num < menor) {//verifica se o num gerado é menor que menor que foi definido como 100, se for o menor recebe o num gerado ex: num 20 e manor 100, o menor passa a ser 20
             menor = num;
         }
+
+        sm= (sm+num);
     }
 
 }
@@ -30,5 +31,7 @@ console.log('o menor numero é:', menor);
 
 soma = (maior+menor)/2;
 
-console.log('\na media é:', soma);//media do maior e menor
+console.log('\na media é do maior e menor é:', soma);//media do maior e menor
+
+console.log('a media de todos os numeros é',(sm/10),'\n');
 
---
+++ pratica_array.js
@@ -0,0 +1,26 @@
+const number= [];
+const a =[];
+
+cont = 0
+
+while (cont < 10) {//gera numeros aleatorios de 0 a 100, e verifica se o numero gerado é maior ou menor que os numeros gerados anteriormente, se for maior ele substitui o maior, se for menor ele substitui o menor
+    let gerados = Math.floor(Math.random() * 100);
+
+number.push(gerados);
+    cont++;
+ 
+       
+}
+console.log(number);   
+
+let menor = Math.min(...number); // menor numero do array
+let maior = Math.max(...number); // maior numero do array
+console.log('\nmenor', menor); 
+console.log('maior', maior); 
+
+let soma = (menor + maior)/2;
+console.log('media do menor e maior', soma); 
+
+
+
+
