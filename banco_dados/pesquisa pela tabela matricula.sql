95a679e5b47f0921cd26d94fa110a7c0a296173c
Csantilucas <lucascsanti2006@gmail.com>
2025-07-04 17:43:22 -0400

    bd

--- banco_dados/pesquisa pela tabela matricula.sql
+++ banco_dados/pesquisa pela tabela matricula.sql
@@ -30,3 +30,42 @@ WHERE
         AND FK_aluno = PK_aluno
         AND FK_cadastro = PK_cadastro
 ORDER BY PK_curso , pess_nome;
+
+
+
+# pesquisando o nome do professor  e o nome do curo pela matricula 
+
+SELECT
+    PK_matricula AS 'ID da Matrícula',
+    curs_nome AS 'Nome do Curso',
+    pess_nome AS 'Nome do Professor'
+FROM
+    tb_matricula,
+    tb_turma ,
+    tb_curso ,
+    tb_professor,
+    tb_cadastro_pessoa
+WHERE
+    FK_turma = PK_turma AND
+    tb_turma.FK_curso = PK_curso AND
+    FK_professor = PK_professor AND
+    FK_cadastro = PK_cadastro
+ORDER BY
+PK_matricula;
+
+#pesquisa nome e endereco
+select 
+pess_nome, pess_endereco
+from
+tb_matricula,
+tb_aluno,
+tb_cadastro_pessoa,
+tb_endereco
+where 
+FK_aluno = PK_aluno and
+tb_cadastro_pessoa.FK_endereco = PK_endereco and
+fk_cadastro = PK_cadastro;
+
+
+
+    
